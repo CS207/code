7f9eec86b9da0931b58d5b742775afa2a3d2ee16
lang1628 <493973966@qq.com>
2015-10-16 23:19:47 -0600

    Update project.ino

--- project.ino
+++ project.ino
@@ -4,18 +4,36 @@ const int motor_pin_left = 6
 const int PhotoresistorPin = A1;
 const int TemperaturePin = A2;
 
+float Photoresistor,Temperature;
+int right,left;
+
+int high = 255;
 void setup() {
   Serial.begin(9600);
   pinMode(motor_pin_right,OUTPUT);
   pinMode(motor_pin_right,OUTPUT);
-  pinMode(PhotoresistorPin,INPUT);
-  pinMode(TemperaturePin,INPUT);
 
 }
 
 void loop() {
-  analogRead(PhotoresistorPin);
-  analogRead(TemperaturePin);
-  
+  Photoresistor = analogRead(PhotoresistorPin);
+  Temperature = analogRead(TemperaturePin);
+
+  if()
+  {
+    analogWrite(motor_pin_right, Speed); 
+    delay(onTime);
+  }
+  else if()
+  {
+    analogWrite(motor_pin_left, Speed); 
+    delay(onTime);
+  }
+  else
+  {
+    analogWrite(motor_pin_right, high); 
+    analogWrite(motor_pin_left, high); 
+    delay(1000);
+  }
 
 }
